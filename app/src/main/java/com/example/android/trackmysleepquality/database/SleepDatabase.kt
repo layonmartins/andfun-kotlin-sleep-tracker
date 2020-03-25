72e443371bc21aded0e7b15b7a5d59df421bfe55
Layon Martins <layonmartins03@gmail.com>
2020-03-25 14:06:02 -0300

    Exercise 04 - Creating a Roomo Database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,16 +16,52 @@
 
 package com.example.android.trackmysleepquality.database
 
-// TODO (01) Create an abstract class that extends RoomDatabase.
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-// TODO (02) Declare an abstract value of type SleepDatabaseDao.
+// TODOne (01) Create an abstract class that extends RoomDatabase.
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
 
-// TODO (03) Declare a companion object.
+    // TODOne (02) Declare an abstract value of type SleepDatabaseDao.
+    abstract val sleepDatabaseDao: SleepDatabaseDao
 
-// TODO (04) Declare a @Volatile INSTANCE variable.
+    // TODOne (03) Declare a companion object.
+    companion object {
+        // TODOne (04) Declare a @Volatile INSTANCE variable.
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
 
-// TODO (05) Define a getInstance() method with a synchronized block.
+        // TODOne (05) Define a getInstance() method with a synchronized block.
+        fun getInstance(context: Context): SleepDatabase {
+            // TODOne (06) Inside the synchronized block:
+            synchronized(this) {
+                var instance = INSTANCE
+                // Check whether the database already exists,
+                // and if it does not, use Room.databaseBuilder to create it.
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 
-// TODO (06) Inside the synchronized block:
-// Check whether the database already exists,
-// and if it does not, use Room.databaseBuilder to create it.
+}
+
+
+
+
+
+
+
+
+
+
